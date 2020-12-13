fbd027be9b86a4cecbba05639be7e236a6a0afd4
naaarizqi <70641890+naaarizqi@users.noreply.github.com>
2020-12-13 16:23:49 +0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var n = prompt("Let Me Know Your Name:");
+var c = confirm("Hello " + n + " Welcome to Glow Up With Me");
+if (c == true) {
+    alert('Hello, Get Ready For Look At Glow Up With Me Website');
+} else {
+    alert('No Problem');
+}
